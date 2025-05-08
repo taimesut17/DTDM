c8448b06b481ee89e6c67548cec08c704939d774
TaiMesut <nguyenthanhtaiabcxyz@gmail.com>
2025-05-09 02:17:34 +0700

    up

--- src/main/java/com/mesut/configs/SpringSecurityConfigs.java
+++ src/main/java/com/mesut/configs/SpringSecurityConfigs.java
@@ -71,7 +71,7 @@ public class SpringSecurityConfigs {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization"));
